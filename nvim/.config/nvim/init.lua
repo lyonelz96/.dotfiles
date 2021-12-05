6747a53d733c31a147bb62ea205a0c45b454b1ea
Lyonel Zamora <lyonelz516@gmail.com>
2021-12-04 20:18:46 -0500

    Comment mappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -238,12 +238,12 @@ require('which-key').setup({})
 
 --- COMMENT ---
 require('Comment').setup({
-    toggler = {
-        line = '<leader>gcc',
-        block = '<leader>gbc',
-    },
-    opleader = {
-        line = '<leader>gc',
-        block = '<leader>gb',
-    }
+	toggler = {
+		line = '<leader>gcc',
+		block = '<leader>gbc',
+	},
+	opleader = {
+		line = '<leader>gc',
+		block = '<leader>gb',
+	},
 })
