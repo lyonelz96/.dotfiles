c8baf5e8b136aafcba1e9a07b23ae10c72714bee
Lyonel Zamora <lyonelz516@gmail.com>
2022-06-27 07:00:05 -0400

    organize plugins/init.lua

--- nvim/.config/nvim/lua/homey/plugins/init.lua
+++ nvim/.config/nvim/lua/homey/plugins/init.lua
@@ -1,15 +1,23 @@
 require('homey.plugins.configs.packer')
+
+--- LSP ---
 require('homey.plugins.configs.lsp')
 require('homey.plugins.configs.cmp')
 require('homey.plugins.configs.lsp_signature')
 require('homey.plugins.configs.null_ls')
 require('homey.plugins.configs.luasnip')
-require('homey.plugins.configs.telescope')
-require('homey.plugins.configs.nvim_tree')
-require('homey.plugins.configs.lualine')
+
+--- QOL ---
 require('homey.plugins.configs.autopairs')
-require('homey.plugins.configs.barbar')
 require('homey.plugins.configs.lightbulb')
 require('homey.plugins.configs.which_key')
 require('homey.plugins.configs.comment')
+
+--- LOOKS ---
+require('homey.plugins.configs.lualine')
+require('homey.plugins.configs.barbar')
 require('homey.plugins.configs.treesitter')
+
+--- NAVIGATION ---
+require('homey.plugins.configs.telescope')
+require('homey.plugins.configs.nvim_tree')
