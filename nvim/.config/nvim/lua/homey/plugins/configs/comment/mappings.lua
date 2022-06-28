04582b31af2715f6e5983ddb0528c72eb0c05e29
Lyonel Zamora <lyonelz516@gmail.com>
2022-06-28 10:04:22 -0400

    add comment count keymap

--- nvim/.config/nvim/lua/homey/plugins/configs/comment/mappings.lua
+++ nvim/.config/nvim/lua/homey/plugins/configs/comment/mappings.lua
@@ -1,5 +1,7 @@
 vim.keymap.set('n', '<leader>cl', require('Comment.api').toggle_current_linewise, { desc = '[C]omment [L]ine' })
 
+vim.keymap.set('n', '<leader>cc', require('Comment.api').toggle_linewise_count, { desc = '[C]omment [C]ount' })
+
 vim.keymap.set('v', '<leader>cb', function()
 	local key = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
 	vim.api.nvim_feedkeys(key, 'nx', false)
