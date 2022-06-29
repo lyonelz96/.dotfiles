3e6ce9f9581f8d51fac4a6f391838c64fe850da3
Lyonel Zamora <lyonelz516@gmail.com>
2022-06-29 06:28:57 -0400

    add operator pending comment keymap

--- nvim/.config/nvim/lua/homey/plugins/configs/comment/mappings.lua
+++ nvim/.config/nvim/lua/homey/plugins/configs/comment/mappings.lua
@@ -1,6 +1,11 @@
-vim.keymap.set('n', '<leader>cl', require('Comment.api').toggle_current_linewise, { desc = '[C]omment [L]ine' })
+vim.keymap.set('n', '<leader>cl', require('Comment.api').toggle_current_linewise, { desc = '[C]omment Current [L]ine' })
 
 vim.keymap.set('n', '<leader>cc', require('Comment.api').toggle_linewise_count, { desc = '[C]omment [C]ount' })
+
+vim.keymap.set('n', '<leader>co', function()
+	require('Comment.api').call('toggle_linewise_op')
+	vim.api.nvim_feedkeys('g@', 'n', false)
+end, { desc = '[C]omment [O]perator' })
 
 vim.keymap.set('v', '<leader>cb', function()
 	local key = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
