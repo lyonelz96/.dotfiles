84736222ddaac8e18a51fec4d20f338b4307369e
Lyonel Zamora <lyonelz516@gmail.com>
2022-06-29 04:46:24 -0400

    change some cmp mappings

--- nvim/.config/nvim/lua/homey/plugins/configs/cmp.lua
+++ nvim/.config/nvim/lua/homey/plugins/configs/cmp.lua
@@ -9,10 +9,10 @@ cmp.setup({
 	mapping = {
 		['<C-n>'] = cmp.mapping.select_next_item(),
 		['<C-p>'] = cmp.mapping.select_prev_item(),
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.close(),
+		['<C-x>'] = cmp.mapping.close(),
 		['<CR>'] = cmp.mapping.confirm({ select = false }),
 	},
 	sources = {
