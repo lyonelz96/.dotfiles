f54d0bc46b89900ea5c31ac5daf6b41f8042bad3
Lyonel Zamora <lyonelz516@gmail.com>
2022-06-30 00:26:09 -0400

    remove lsp_signature require

--- nvim/.config/nvim/lua/homey/plugins/init.lua
+++ nvim/.config/nvim/lua/homey/plugins/init.lua
@@ -3,7 +3,6 @@ require('homey.plugins.configs.packer')
 --- LSP ---
 require('homey.plugins.configs.lsp')
 require('homey.plugins.configs.cmp')
-require('homey.plugins.configs.lsp_signature')
 require('homey.plugins.configs.null_ls')
 
 --- QOL ---
