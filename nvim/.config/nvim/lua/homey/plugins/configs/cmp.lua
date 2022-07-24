997b70845df6060e4cc800af6c6985d8c557c927
Lyonel Zamora <lyonelz516@gmail.com>
2022-07-23 21:37:31 -0400

    change cmp mappings for luasnip

--- nvim/.config/nvim/lua/homey/plugins/configs/cmp.lua
+++ nvim/.config/nvim/lua/homey/plugins/configs/cmp.lua
@@ -12,20 +12,18 @@ cmp.setup({
 		documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
-		['<C-n>'] = cmp.mapping(function()
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
+		['<C-n>'] = cmp.mapping.select_next_item(),
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+		['<C-l>'] = cmp.mapping(function()
+			if luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
 			end
-		end),
-		['<C-p>'] = cmp.mapping(function()
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
+		end, { 'i', 's' }),
+		['<C-h>'] = cmp.mapping(function()
+			if luasnip.jumpable(-1) then
 				luasnip.jump(-1)
 			end
-		end),
+		end, { 'i', 's' }),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-Space>'] = cmp.mapping.complete(),
