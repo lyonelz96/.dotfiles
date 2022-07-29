05aca772b892759e868fd15768c52d7379c7ab17
Lyonel Zamora <lyonelz516@gmail.com>
2022-07-29 16:03:05 -0400

    auto close nvim tree

--- nvim/.config/nvim/lua/homey/plugins/configs/nvim_tree/init.lua
+++ nvim/.config/nvim/lua/homey/plugins/configs/nvim_tree/init.lua
@@ -2,6 +2,11 @@ require('nvim-tree').setup({
 	diagnostics = {
 		enable = true,
 	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
+	},
 })
 
 require('homey.plugins.configs.nvim_tree.mappings')
