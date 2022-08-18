636118031424970de1a4f2db29de298cf9ddb55c
Lyonel Zamora <lyonelz516@gmail.com>
2022-08-18 17:20:55 -0400

    add new lua api to comment mappings

--- nvim/.config/nvim/lua/homey/plugins/configs/comment/mappings.lua
+++ nvim/.config/nvim/lua/homey/plugins/configs/comment/mappings.lua
@@ -1,14 +1,18 @@
-vim.keymap.set('n', '<leader>cl', require('Comment.api').toggle_current_linewise, { desc = '[C]omment Current [L]ine' })
+vim.keymap.set('n', '<leader>cl', require('Comment.api').toggle.linewise.current, { desc = '[C]omment Current [L]ine' })
 
-vim.keymap.set('n', '<leader>cc', require('Comment.api').toggle_linewise_count, { desc = '[C]omment [C]ount' })
+vim.keymap.set('n', '<leader>cc', function()
+	require('Comment.api').toggle.linewise.count(vim.v.count)
+end, { desc = '[C]omment [C]ount' })
 
-vim.keymap.set('n', '<leader>co', function()
-	require('Comment.api').call('toggle_linewise_op')
-	vim.api.nvim_feedkeys('g@', 'n', false)
-end, { desc = '[C]omment [O]perator' })
+vim.keymap.set(
+	'n',
+	'<leader>co',
+	require('Comment.api').call('toggle.linewise', 'g@'),
+	{ desc = '[C]omment [O]perator', expr = true }
+)
 
 vim.keymap.set('v', '<leader>cb', function()
 	local key = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
 	vim.api.nvim_feedkeys(key, 'nx', false)
-	require('Comment.api').toggle_linewise_op(vim.fn.visualmode())
+	require('Comment.api').toggle.linewise(vim.fn.visualmode())
 end, { desc = '[C]omment [B]lock' })
