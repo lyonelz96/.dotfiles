3282c15541d8b076a44be721ec5a5e8073733f5f
Lyonel Zamora <lyonelz516@gmail.com>
2022-10-07 01:05:29 -0400

    add lsp_formatting function to mappings

--- nvim/.config/nvim/lua/homey/plugins/configs/lsp/mappings.lua
+++ nvim/.config/nvim/lua/homey/plugins/configs/lsp/mappings.lua
@@ -9,6 +9,40 @@ M.set_lsp_mappings = function(bufnr)
 		vim.keymap.set('i', keys, func, { buffer = bufnr, desc = desc })
 	end
 
+	local lsp_formatting = function()
+		local function on_choice(choice)
+			if choice == nil then
+				return nil
+			end
+
+			print('Formatted with: ' .. choice.name)
+			vim.lsp.buf.format({ bufnr = bufnr, name = choice.name })
+		end
+
+		local clients = vim.lsp.get_active_clients()
+		local clients_with_formatting = vim.tbl_filter(function(client)
+			return client['server_capabilities']['documentFormattingProvider']
+		end, clients)
+
+		table.sort(clients_with_formatting, function(a, b)
+			return a.name < b.name
+		end)
+
+		if #clients_with_formatting > 1 then
+			vim.ui.select(clients_with_formatting, {
+				prompt = 'Select a language server:',
+				format_item = function(client)
+					return client.name
+				end,
+			}, on_choice)
+		elseif #clients_with_formatting < 1 then
+			print('No clients with formatting capabilities attached')
+			on_choice(nil)
+		else
+			on_choice(clients_with_formatting[1])
+		end
+	end
+
 	nmap('<leader>lgd', vim.lsp.buf.definition, '[L]SP [G]oto [D]efinition')
 	nmap('<leader>lgi', vim.lsp.buf.implementation, '[L]SP [G]oto [I]mplementation')
 	nmap('<leader>lh', vim.lsp.buf.hover, '[L]SP [H]over')
@@ -16,7 +50,7 @@ M.set_lsp_mappings = function(bufnr)
 	imap('<C-s>', vim.lsp.buf.signature_help, '[L]SP [S]ignature [H]elp')
 	nmap('<leader>lrn', vim.lsp.buf.rename, '[L]SP [R]e[N]ame')
 	nmap('<leader>lca', vim.lsp.buf.code_action, '[L]SP [C]ode [A]ction')
-	nmap('<leader>lf', vim.lsp.buf.formatting, '[L]SP [F]ormat')
+	nmap('<leader>lf', lsp_formatting, '[L]SP [F]ormat')
 
 	nmap('<leader>tlr', require('telescope.builtin').lsp_references, '[T]elescope [L]SP [R]eferences')
 	nmap('<leader>tlsd', require('telescope.builtin').lsp_document_symbols, '[T]elescope [L]SP [S]ymbols [D]ocument ')
