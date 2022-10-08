b39d82c6ebf1fbb0aed3656544fccb827add4366
Lyonel Zamora <lyonelz516@gmail.com>
2022-10-08 13:02:21 -0400

    change PackerCompile to PackerSync

--- nvim/.config/nvim/lua/homey/plugins/configs/packer.lua
+++ nvim/.config/nvim/lua/homey/plugins/configs/packer.lua
@@ -69,7 +69,7 @@ end
 
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
-	command = 'source <afile> | PackerCompile',
+	command = 'source <afile> | PackerSync',
 	group = packer_group,
 	pattern = 'packer.lua',
 })
