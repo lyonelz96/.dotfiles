893da69c7757e87c8abe654883f8b9609c1fff3d
Lyonel Zamora <lyonelz516@gmail.com>
2022-10-13 01:07:44 -0400

    change bufferline pickclose mapping

--- nvim/.config/nvim/lua/homey/plugins/configs/bufferline/mappings.lua
+++ nvim/.config/nvim/lua/homey/plugins/configs/bufferline/mappings.lua
@@ -3,7 +3,7 @@ vim.keymap.set('n', '<leader>bcl', '<Cmd>BufferLineCloseRight<CR>')
 vim.keymap.set('n', '<leader>bh', '<Cmd>BufferLineCyclePrev<CR>')
 vim.keymap.set('n', '<leader>bl', '<Cmd>BufferLineCycleNext<CR>')
 vim.keymap.set('n', '<leader>bp', '<Cmd>BufferLinePick<CR>')
-vim.keymap.set('n', '<leader>bpc', '<Cmd>BufferLinePickClose<CR>')
+vim.keymap.set('n', '<leader>bcp', '<Cmd>BufferLinePickClose<CR>')
 
 require('which-key').register({
 	b = {
