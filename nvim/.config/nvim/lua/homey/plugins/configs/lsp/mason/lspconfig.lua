0683c600b94675ceac13bc50d6b91a3e1c3fb882
Lyonel Zamora <lyonelz516@gmail.com>
2022-10-16 21:19:54 -0400

    update nvim cmp

--- nvim/.config/nvim/lua/homey/plugins/configs/lsp/mason/lspconfig.lua
+++ nvim/.config/nvim/lua/homey/plugins/configs/lsp/mason/lspconfig.lua
@@ -4,7 +4,7 @@ local on_attach = function(_, bufnr)
 	require('homey.plugins.configs.lsp.mappings').set_lsp_mappings(bufnr)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require('mason-lspconfig').setup_handlers({
 	-- default handler
